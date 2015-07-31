8fbc841d8edd549a9653d9b387e08f67bbfd7141
Peter Dalmaris <peter@futureshock.com.au>
2015-07-31 13:46:40 +1000

    Added header comments

--- lab_app_v2.py
+++ lab_app_v2.py
@@ -1,3 +1,46 @@
+'''
+FILE NAME
+lab_app.py
+Version 2
+
+1. WHAT IT DOES
+Implements the first version of the project's Flask application.
+This version contains adds a page that displays sensor readings from the database.
+ 
+2. REQUIRES
+* Any Raspberry Pi
+
+3. ORIGINAL WORK
+Raspberry Full Stack 2015, Peter Dalmaris
+
+4. HARDWARE
+* Any Raspberry Pi
+* DHT11 or 22
+* 10KOhm resistor
+* Breadboard
+* Wires
+
+5. SOFTWARE
+Command line terminal
+Simple text editor
+Libraries:
+from flask import Flask, request, render_template, sqlite3
+
+6. WARNING!
+None
+
+7. CREATED 
+
+8. TYPICAL OUTPUT
+A simple web page served by this flask application in the user's browser.
+The page contains the current temperature and humidity.
+A second page that displays historical environment data from the SQLite3 database.
+
+ // 9. COMMENTS
+--
+ // 10. END
+'''
+
 from flask import Flask, request, render_template
 app = Flask(__name__)
 app.debug = True # Make this False if you are no longer debugging
