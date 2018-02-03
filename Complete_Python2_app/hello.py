f28dff7d0a19e344b62221cb128aea11789ab0e2
Peter Dalmaris <peter@futureshock.com.au>
2018-02-03 19:24:12 +1100

    Added complete Python 2 app files.

---
+++ Complete_Python2_app/hello.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+app.debug = True
+
+
+@app.route("/")
+def hello():
+	return render_template('vhello.html', message="Hello World!")
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
